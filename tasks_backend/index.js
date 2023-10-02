02666470f00a06735438864173ddefdf0799f380
Nicolas Nagano <nicolasjap@hotmail.com>
2023-10-02 00:14:22 -0300

    backend_finalizado

--- tasks_backend/index.js
+++ tasks_backend/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Meu BackEnd!')
@@ -7,4 +18,4 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
     console.log('BackEnd executando...')
-})
+}) 
